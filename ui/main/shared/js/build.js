cdf41b0e190892dfc8f43f243a59eda31e26038e
Benjamin Rianda <reptarforlife@gmail.com>
2015-01-10 06:56:26 -0800

    power added to lists

--- ui/main/shared/js/build.js
+++ ui/main/shared/js/build.js
@@ -4,11 +4,11 @@ function BuildHotkeyModel() {
 
     self.SpecIdToGridMap = ko.observable(
         {
-            "/pa/units/land/radar/radar.json": ["utility", 7],
-
+            "/pa/units/land/radar/radar.json": ["factory", 12],
             "/pa/units/land/bot_factory/bot_factory.json": ["factory", 13],
-    
-            "/pa/units/land/assault_bot/assault_bot.json": ["bot", 11],
+			"/pa/units/land/energy_plant/energy_plant.json": ["factory", 14],
+
+			"/pa/units/land/assault_bot/assault_bot.json": ["bot", 11],
             "/pa/units/land/fabrication_bot_combat/fabrication_bot_combat.json": ["bot", 13],
         }
     );
