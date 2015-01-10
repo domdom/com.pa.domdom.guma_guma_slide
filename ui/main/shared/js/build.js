7ffab095c19f4635a182d883786d7b7724733f25
domdom <domdom314@gmail.com>
2015-01-11 00:17:05 +1000

    tidy up

--- ui/main/shared/js/build.js
+++ ui/main/shared/js/build.js
@@ -4,7 +4,7 @@ function BuildHotkeyModel() {
 
     self.SpecIdToGridMap = ko.observable(
         {
-            "/pa/units/land/radar/radar.json": ["utility", 7],          
+            "/pa/units/land/radar/radar.json": ["utility", 7],
 
             "/pa/units/land/bot_factory/bot_factory.json": ["factory", 13],
     
